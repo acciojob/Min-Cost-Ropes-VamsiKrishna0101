b05673a700008d27c2d88a655fe17cf2ddb79d17
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-01 17:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mincost(arr)
 //write your code here
 	let mini=0
 	arr.sort((a,b)=>a-b)
-	while(arr.length>0){
+	while(arr.length>1){
 		let first=arr.shift()
 		let second=arr.shift()
 		let cost=first+second
